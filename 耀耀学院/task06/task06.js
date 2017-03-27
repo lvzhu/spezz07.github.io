d7e9bfe8f87d75944d033964912dc289551f62a0
spezz07 <spezz123@126.com>
2017-03-28 00:08:37 +0800

    UI组件之浮出层

---
+++ 耀耀学院/task06/task06.js
@@ -0,0 +1,43 @@
+(function () {
+    function $(selector, context) {
+        if (context) {
+            return context.querySelector(selector);
+        }
+        return document.querySelector(selector);
+    }
+    const cover = $(".cover");
+    const layer =$(".fl-layer");
+    const btn = $(".layer-show");
+    const layerSize =  {w:$(".layer-w"), h:$(".layer-h")};
+    function openlayer() {
+        let bodyW = document.documentElement.scrollWidth;
+        let bodyH =  document.documentElement.scrollHeight;
+        let browerW = document.documentElement.clientWidth;
+        let browerH = document.documentElement.clientHeight;
+        let layerW = layerSize.w.value==="" ? 400 : $(".layer-w").value;
+        let layerH = layerSize.h.value==="" ? 200 : $(".layer-h").value;
+        cover.style.width = bodyW+"px";
+        cover.style.height = bodyH+"px";
+        cover.style.display = "block";
+        layer.style.display = "block";
+        layer.style.width = layerW===400 ? 400+"px":layerW+"px";
+        layer.style.height = layerH===200 ? 200+"px":layerH+"px";
+        layer.style.left = (browerW/2)-(layerW/2)+"px";
+        layer.style.top = (browerH/2)-(layerH/2)+"px";
+    }
+    function closelayer(e) {
+        let ev = e||window.event;
+        if(ev.target.className==="cover"){
+            cover.style.display = "none";
+            layer.style.display = "none"
+        }
+        else {
+            return false;
+        }
+    }
+    cover.onclick = function (e) {
+        closelayer(e)
+    };
+    btn.onclick = openlayer;
+
+})();
