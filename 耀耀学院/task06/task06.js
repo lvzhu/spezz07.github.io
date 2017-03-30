35922ad39695c38352caa2afb7e325be0d5aaa1e
spezz07 <spezz123@126.com>
2017-03-30 23:57:07 +0800

    添加拖拽方法

--- 耀耀学院/task06/task06.js
+++ 耀耀学院/task06/task06.js
@@ -20,6 +20,7 @@
         cover.style.height = bodyH+"px";
         cover.style.display = "block";
         layer.style.display = "block";
+        layer.style.position = "absolute";
         layer.style.width = layerW===400 ? 400+"px":layerW+"px";
         layer.style.height = layerH===200 ? 200+"px":layerH+"px";
         layer.style.left = (browerW/2)-(layerW/2)+"px";
@@ -39,31 +40,81 @@
         closelayer(e)
     };
     btn.onclick = openlayer;
-    function drag(elem) {
-        function isTransform() {
-            let transform ;
-            let transformArray = ['transform', 'webkitTransform', 'MozTransform', 'msTransform', 'OTransform'];
-            let tansformdiv = document.createElement("div").style;
-            for(let i=0;i<transformArray.length;i++){
-                if (transformArray[i] in tansformdiv){
-                    console.log(transformArray[i]);
-                    return  transform = transformArray[i]
-                }
-                else {
-                    return false
-                }
-            }
-        }
+    let drag = function(elem,e) {
+        let pos = {x:0,y:0};
+        let mstartpos = {x:0,y:0};
+        let ev=e||window.event;
         function getStyle(elem, property) {
             // ie通过currentStyle来获取元素的样式，其他浏览器通过getComputedStyle来获取
             return document.defaultView.getComputedStyle ? document.defaultView.getComputedStyle(elem, false)[property] : elem.currentStyle[property];
         }
-        function elemStartPos(elem) {
-             if(isTransform()){
-
+        function elemPos(elem) {
+            let elemPosition = getStyle(elem,"position");
+            if(elemPosition==="static") {
+                elem.style.position = "absolute";
+                return pos;
+            }
+            else {
+                let x = parseInt(getStyle(elem,"left"));
+                let y = parseInt(getStyle(elem,"top"));
+                return pos = {x,y}
+            }
+            /*暂时只用positon定位
+             function isTransform() {
+             let transform ;
+             let transformArray = ['transform', 'webkitTransform', 'MozTransform', 'msTransform', 'OTransform'];
+             let tansformdiv = document.createElement("div").style;
+             for(let i=0;i<transformArray.length;i++){
+             if (transformArray[i] in tansformdiv){
+             console.log(transformArray[i]);
+             return  transform = transformArray[i]
              }
+             else {
+             return false
+                }
+                }
+             }
+             if(isTransform()){
+             let transformName = isTransform();
+             let elemTransform = getStyle(elem,transformName);
+             if(elemTransform==="none"){
+             elem.style[transformName] = "transform(0,0)"
+             }
+             else {
+             let reg = /-?\d+/g;
+             let t = elemTransform.match(reg);//因为如果transfrom有数值的话，返回的是matrix矩阵 比如返回值是matrix(1,0,0,20,50) 其中,20和50分别为 translateX和Y的值,获取和设置的就是这个
+             let x = parseInt(t[4]);
+             let y = parseInt(t[5]);
+             return pos = {x,y}
+             }
+             }*/
         }
-    }
+        function start(ev) {
+            let mouseStartX = ev.pageX;
+            let mouseStartY = ev.pageY;
+            elemPos(elem);
+            mstartpos.x = mouseStartX;
+            mstartpos.y = mouseStartY;
 
+            elem.addEventListener("mouseup",end,false);
+            elem.addEventListener('mousemove',move,false);
+        }
+        function setElemPos(elem,disX,disY) {
+            elem.style.top = disY +"px";
+            elem.style.left = disX +"px"
+        }
+        function move(ev) {
+            let distanceX = ev.pageX - mstartpos.x;
+            let distanceY = ev.pageY - mstartpos.y;
+            setElemPos(elem,(pos.x+distanceX),(pos.y+distanceY))
+        }
+        function end() {
+            elem.removeEventListener('mousemove',move,false);
+            elem.removeEventListener('mouseup',end,false);
+        }
 
+        return{move,start,end}
+    };
+    layer.addEventListener("mousedown",drag(layer).start,false);
+    layer.addEventListener("mouseup",drag(layer).end,false)
 })();
